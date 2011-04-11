e6fe032f8bf5ce9bdde66d2451d0ec73a307ac93
Adrian Perez <aperez@igalia.com>
2011-04-12 00:40:23 +0300

    Add sane cache defaults and ensure directory creation
    Ensures that the configuration for the cache hook module is sane,
    and that the cache directory is created early in the execution of
    Growl. This avoids crashes e.g. just after making a fresh clone of
    the repository.

--- _hooks/config.py
+++ _hooks/config.py
@@ -16,3 +16,13 @@ for x in glob( os.path.join(Site.BASE_DIR, "_config") + "/*.conf" ):
         basename = os.path.basename(x).split(".")[0]
         Site.CONTEXT.config[basename] = cp.ConfigParser()
         Site.CONTEXT.config[basename].readfp(f)
+
+# Set sane cache config defaults
+if not Site.CONTEXT.config.cache.has_option("cache", "cache_dir"):
+    Site.CONTEXT.config.cache.set("cache", "cache_dir", "_cache")
+
+if not Site.CONTEXT.config.cache.has_option("cache", "expires"):
+    Site.CONTEXT.config.cache.set("cache", "expires", 1800)
+
+# Ensure that the cache directory exists
+os.makedirs(Site.CONTEXT.config.cache.get("cache", "cache_dir"))
