c72eaf70812d663c5a4d76f1825e382b2b29f1ab
ajdiaz <ajdiaz@dotscale.com>
2012-07-15 11:47:41 +0200

    Add fix for non 200 returned code on innovation

--- _mods/innovation.py
+++ _mods/innovation.py
@@ -48,6 +48,9 @@ def get_articles(area):
 
         if not blog:
             blog = feedparser.parse(feed)
+            if blog["status"] != 200:
+                print "warning:innovation:%s:feed %s return a non valid status. Skipping..." % (area, feed)
+                continue
             cache.set(cache_key, blog)
 
         if len(blog.feed) == 0:
