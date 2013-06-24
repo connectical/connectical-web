fd4b810498a8ed08b17ea0893fc777a08c13f975
Andrés J. Díaz <ajdiaz@connectical.com>
2013-06-24 11:27:54 +0200

    Remove some unused plugins.

--- _libs/plugin/identica.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- encoding: utf-8 -*-
-# vim:fenc=utf-8:
-
-identica_url = "http://identi.ca/api/statuses/user_timeline.json?screen_name=%s&count=5"
-
-def get(user):
-    return identica_url % user
-
